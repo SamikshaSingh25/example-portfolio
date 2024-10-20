a4f806163b25d554f4808a8a2b99f2d25c42a7cf
Samiksha Singh <123529290+SamikshaSingh25@users.noreply.github.com>
2024-10-20 21:21:34 +0530

    Add files via upload

---
+++ portfolio/form.js
@@ -0,0 +1,22 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  
+    
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    
+    var csvContent = "data:text/csv;charset=utf-8,"
+      + "Name,Email,Message\n"
+      + name + "," + email + "," + message;
+
+    
+    var link = document.createElement("a");
+    link.href = encodeURI(csvContent);
+    link.download = "form_data.csv";
+    document.body.appendChild(link);
+
+  link.click();
+    form.reset();
+    
+  });
